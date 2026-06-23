6d0dcbcdc1fab590740dac7fca18917eaf771f53
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-06-23 21:37:45 +0200

    First section of learning materials

---
+++ people.js
@@ -0,0 +1,5 @@
+const people = ['jfg', 'fjg', 'gkjf']
+const ages = [43, 32,8]
+console.log('module', people)
+
+module.exports = {people, ages}
---
+++ test.js
@@ -0,0 +1,11 @@
+// const name = 'mario'
+// console.log(name)
+
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(document.querySelector)
+
+const {people, ages} = require('./people')
+
+console.log('now    ',people)
+console.log('age   ', ages)
