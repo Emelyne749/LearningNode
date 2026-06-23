05b58767c5cd96367aecd7eb74c121c575efdaaf
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-06-23 23:12:02 +0200

    Working on streams and pipes with data sharing

--- streams.js
+++ streams.js
@@ -4,10 +4,17 @@ const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf8'})
 
 const writeStream = fs.createWriteStream('./docs/blog4.txt')
 
-readStream.on('data',(chunk)=>{
-    console.log('-------------- New chunk --------------------')
-    writeStream.write('\n\n\n\nNEW CHUNK\n\n\n\n');
-    writeStream.write(chunk)
-})
+// readStream.on('data',(chunk)=>{
+//     console.log('-------------- New chunk --------------------')
+//     writeStream.write('\n\n\n\nNEW CHUNK\n\n\n\n');
+//     writeStream.write(chunk)
+// })
 
 //we can use to string or add a data type argument to the create readsteam 
+
+
+// Piping
+
+
+
+readStream.pipe(writeStream)
